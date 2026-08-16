3de20ff124522c88250f05269b2027d3da00846e
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-16 12:22:34 +0100

    Add docstring to __init__.py

--- src/superlists/urls.py
+++ src/superlists/urls.py
@@ -3,7 +3,9 @@ URL configuration for superlists project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
+
+Examples
+--------
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
@@ -14,9 +16,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls)]
