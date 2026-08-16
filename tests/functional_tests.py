a20ce8e0adad8d8ecc1fe95e99468b2d035c4c97
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-16 11:33:13 +0100

    Set up Django project

---
+++ tests/functional_tests.py
@@ -0,0 +1,9 @@
+"""Test Django homepage."""
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get("http://localhost:8000")
+
+assert "Congratulations!" in browser.title
+print("OK")
