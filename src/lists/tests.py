95086bf0160eed66c8a03ea814134d0a1afdce99
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-17 22:40:40 +0100

    url config, map / to home_page view

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -1,9 +1,6 @@
 """Testing lists app."""
 
-from django.http import HttpRequest
 from django.test import TestCase
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
@@ -11,9 +8,7 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self) -> None:
         """Test return correct html for home page."""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
--- src/superlists/urls.py
+++ src/superlists/urls.py
@@ -20,4 +20,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+from lists.views import home_page
+
+urlpatterns = [path("admin/", admin.site.urls), path("", home_page, name="home")]
