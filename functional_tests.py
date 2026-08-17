69e98972ea179e1eb64722c964be3bcb9f82e612
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-17 23:40:17 +0100

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -47,7 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements(By.TAG_NAME, "tr")
         self.assertTrue(
             any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in table",
+            "New to-do item did not appear in table.",
         )
 
         # There is still a text box inviting her to add another item.
