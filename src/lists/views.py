ab465a273794a6ca9a069b9215282557cde9bfbb
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-17 22:22:53 +0100

    First unit test and view function

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,2 +1,16 @@
 """List views."""
 # Create your views here.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.http import HttpResponse
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+
+
+def home_page(request: HttpRequest) -> HttpResponse:
+    """Home page view."""
+    return HttpResponse("<html><title>To-Do lists</title></html>")
