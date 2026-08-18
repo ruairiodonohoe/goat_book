6670bc8bcd62d2cd8e3c4626cf9f53ee041b00ed
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-18 14:54:34 +0100

    Testing post request

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -11,7 +11,14 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_renders_homepage_content(self) -> None:
-        """Test rendered content of home page."""
+    def test_renders_input_form(self) -> None:
+        """Test rendered input form of home page."""
         response = self.client.get("/")
-        self.assertContains(response, "To-Do")
+        self.assertContains(response, '<form method="post">')
+        self.assertContains(response, '<input name="item_text"')
+
+    def test_can_save_a_post_request(self) -> None:
+        """Test saving POST request."""
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, "home.html")
