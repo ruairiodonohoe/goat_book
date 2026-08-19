dedc614059138a64a3e98a1c09feb605ddee378f
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-08-19 23:42:29 +0100

    Add url for lists/

--- src/lists/views.py
+++ src/lists/views.py
@@ -18,7 +18,7 @@ def home_page(request: HttpRequest) -> HttpResponse:
     """Home page view."""
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
 
     items = Item.objects.all()
     return render(request, "home.html", {"items": items})
