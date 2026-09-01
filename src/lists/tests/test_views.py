0e4bc49c70593cb6b71fec148db66cb4834552fc
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-09-01 23:38:50 +0100

    Changed save method

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,13 +1,12 @@
 """Testing lists app."""
 
 from typing import TYPE_CHECKING
-from unittest import skip
 
 import lxml.html
 from django.test import TestCase
 from django.utils import html
 
-from lists.forms import EMPTY_ITEM_ERROR
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
 from lists.models import Item, List
 
 if TYPE_CHECKING:
@@ -158,15 +157,14 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, html.escape(EMPTY_ITEM_ERROR))
 
-    @skip("Skip for now")
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self) -> None:
-        """Test dplicate item validation errors end up on lists page."""
+        """Test duplicate item validation errors end up on lists page."""
         list1 = List.objects.create()
         Item.objects.create(list=list1, text="textey")
 
-        response = self.client.post(f"/lists/{list1.id}", data={"text": "textey"})
+        response = self.client.post(f"/lists/{list1.id}/", data={"text": "textey"})
 
-        expected_error = html.escape("You've already got this in your list")
+        expected_error = html.escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, "list.html")
         self.assertEqual(Item.objects.all().count(), 1)
