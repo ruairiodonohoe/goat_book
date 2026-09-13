569d1257d69b2212aada18002756c2a9f46284c9
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2026-09-13 12:35:13 +0100

    Add ruff

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -52,7 +52,7 @@ class SharingTest(FunctionalTest):
         self.wait_for(lambda: self.assertEqual(list_page.get_list_owner(), "edith@example.com"))
         # He adds an item to the list
         list_page.add_list_item("Hi Edith!")
-        # When Edith refreshes the page, she sees Onesiphorus's addition
+        # When Edith refreshes the page, she sees Onesiphorus's addition.
         self.browser = edith_browser
         self.browser.refresh()
         list_page.wait_for_row_in_list_table("Hi Edith!", 2)
